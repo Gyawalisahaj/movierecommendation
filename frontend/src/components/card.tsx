91359736d39c9d22580dd676d35a92f627936b4d
Gyawalisahaj <sahajgnawali@gmail.com>
2025-07-15 22:08:01 +0545

    movie card

--- frontend/src/components/card.tsx
+++ frontend/src/components/card.tsx
@@ -16,8 +16,9 @@ export default function MovieCard({ movie }: { movie: Movie }) {
     <div className="bg-white shadow-md border border-orange-400 rounded-lg p-4 w-full max-w-md">
       <h2 className="text-2xl font-bold text-orange-800 mb-2">{movie.title}</h2>
       <p><strong>Genre:</strong> {movie.genre}</p>
-      <p><strong>Cast:</strong> {movie.cast}</p>
-      <p><strong>Director:</strong> {movie.director}</p>
+      <div className="bg-orange-400 rounded-2xl"><p><strong>Cast:</strong> {movie.cast}</p> </div>
+      <div className="bg-orange-400 rounded-2xl"> <p><strong>Director:</strong> {movie.director}</p> </div>
+      
       <p><strong>Production:</strong> {movie.production_house}</p>
       <p><strong>Release:</strong> {movie.release_date}</p>
       <p className="mt-2">{movie.plot}</p>
